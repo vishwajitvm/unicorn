41b369f17c9479e7896d51171fe5928de9db559b
vishwajit vm <35070025+vishwajitvm@users.noreply.github.com>
2022-04-13 12:38:03 +0530

    Route Update

--- routes/web.php
+++ routes/web.php
@@ -44,24 +44,6 @@ use App\Http\Controllers\ManageVideosAdminController ;
 use App\Models\gallery ;
 use App\Models\manage_video ;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-/*
-||
-||
-back rout start here
-||
-||
-*/
 Route::group(['middleware' => 'prevent-back-history'],function(){
     // Auth::routes();
     // Route::get('/home', 'HomeController@index');
